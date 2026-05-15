42c272aed5708edde6decabef9712e7ae8b3cb96
Satyam Nayak <nayaksatyampc@gmail.com>
2026-05-16 02:30:38 +0530

    Time: 0 ms (100%), Space: 19.7 MB (87.67%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for(char c: letters){
+            if( c - '0' > target - '0'){
+                return c;
+            }
+            
+        }
+        return letters[0];
+        
+    }
+};
