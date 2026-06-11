573caaa8d53c0ce585e709886a5f5670d406bef1
Satyam Nayak <nayaksatyampc@gmail.com>
2026-06-11 23:36:23 +0530

    Time: 3 ms (3.31%), Space: 7.9 MB (48.48%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,6 +3,8 @@ public:
     bool isPowerOfTwo(int n) {
         if(n<=0)
             return false;
-        return (n&(n-1))==0;       
+        if(n==1)
+            return true;
+        return(n%2==0 && isPowerOfTwo(n/2));
     }
 };
